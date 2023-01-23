a1e54a9aacdfbfc60dbe7adb4d9ed7e6deeffd54
Concedo <Concedo@noreply.com>
2023-01-23 19:33:47 +0800

    third attempt at config sw

--- staging/sw.js
+++ staging/sw.js
@@ -1,1 +1,13 @@
 console.log("Service worker is active - should enable PWA functionality");
+
+self.addEventListener("fetch", function (event) {
+	event.respondWith(
+		fetch(event.request)
+			.catch(function() {
+				return caches.match(event.request)
+			})
+			.then(function (response) {				
+				return response;
+			})
+	)
+})
